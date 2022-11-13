1cae6b8e1ab60644cdb207bcbb25a87d8a47c06d
innakhabirova <iikhabi@mail.ru>
2022-11-13 23:42:08 +0300

    reformat code was fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int beginning_balance=100;
-        int cash=1200;
-        int bonus=0;
-        int limit=1000;
-        if (cash>limit){
-            bonus=cash/100;
+        int beginning_balance = 100;
+        int cash = 1200;
+        int bonus = 0;
+        int limit = 1000;
+        if (cash > limit) {
+            bonus = cash / 100;
         }
-        int closing_balance=cash+beginning_balance+bonus;
-        System.out.println("итоговый баланс="+closing_balance);
+        int closing_balance = cash + beginning_balance + bonus;
+        System.out.println("итоговый баланс=" + closing_balance);
 
 
     }
